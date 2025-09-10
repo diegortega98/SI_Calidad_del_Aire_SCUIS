3792f364aca7ffd7a259658129bccdac18b4184e
Diego Ortega <diegortega_098@hotmail.com>
2025-09-09 23:13:51 -0500

    Añadido archivo de estilos CSS y carga de estilos en la aplicación principal; implementación de carga inicial de datos desde InfluxDB.

--- main_app.py
+++ main_app.py
@@ -1,11 +1,20 @@
 import streamlit as st
+import pathlib
 
 st.set_page_config(page_title="Calidad del Aire de Transporte Público del AMB", page_icon=":material/edit:", layout="wide")
+
+def load_css(file_path):
+    with open(file_path) as f:
+        st.html(f"<style>{f.read()}</style>")
+
+css_path = pathlib.Path("assets/styles.css")
+load_css(css_path)
 
 about_page = st.Page("pages/home.py", title="Acerca de", icon=":material/quick_reference:")
 dashboard_page = st.Page("pages/map.py", title="Mapa", icon=":material/map:")
 analytics_page = st.Page("pages/analytics.py", title="Análisis", icon=":material/analytics:")
 data_table_page = st.Page("pages/table.py", title="Datos", icon=":material/table_view:")
 
-pg = st.navigation([dashboard_page, analytics_page, data_table_page, about_page])
+
+pg = st.navigation([about_page, dashboard_page, analytics_page, data_table_page])
 pg.run()
